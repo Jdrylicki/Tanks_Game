54da0aabe36be8a81572a59bf6d87c29165d0f31
Jeremy Rylicki <jdrylicki@gmail.com>
2019-08-15 10:38:36 -0500

    optimized bullet, with ricochet

--- Tanks/Assets/BulletMovement.cs
+++ Tanks/Assets/BulletMovement.cs
@@ -6,31 +6,38 @@ public class BulletMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 3;
-    private int direction = 1;
-
+    public int numRicochet = 2;
+    
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Move(1);
-        
-    }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        bulletRicochet(transform.position, transform.forward);
 
-    void Move(int dir)
-    {
-        Vector3 movement = transform.forward * dir * speed * Time.deltaTime;
-        rb.MovePosition(rb.position + movement);
     }
-
-    private void OnCollisionEnter(Collision coll)
+  
+    void bulletRicochet(Vector3 pos, Vector3 dir)
     {
-        ContactPoint contact;
-        if (coll.gameObject.name.Contains("Wall"))
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Time.deltaTime * speed + .05f))
         {
-            contact = coll.contacts[0];
+            if (hit.collider.gameObject.name.Contains("Wall") && numRicochet > 0)
+            {
+                Vector3 reflection = Vector3.Reflect(ray.direction, hit.normal);
+                float rot = 90 - Mathf.Atan2(reflection.z, reflection.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, rot, 0);
+                numRicochet--;
+            }
+            else if(numRicochet <= 0)
+                Destroy(this.gameObject);
 
- 
-            rb.velocity = Vector3.Reflect(transform.forward, contact.normal);
+            if (hit.collider.gameObject.name.Contains("Player"))
+            {
+                Destroy(this.gameObject);
+                Destroy(hit.collider.gameObject);
+            }
         }
     }
 
