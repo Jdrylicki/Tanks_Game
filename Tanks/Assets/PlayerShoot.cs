c34cd3758308ca3fa60266ab4adee46e091127c7
Jdrylicki <jdrylicki@gmail.com>
2019-08-28 09:40:25 -0500

    Actual tank

--- Tanks/Assets/PlayerShoot.cs
+++ Tanks/Assets/PlayerShoot.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 { 
-    public Transform bulletPos;
+    public Transform turretPos;
     public GameObject bullet;
-    
+ 
      
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 spawnLoation = bulletPos.position + (bulletPos.forward/2);
+
+        Vector3 spawnLoation = turretPos.position;
         if (Input.GetMouseButtonDown(0))
-            Instantiate(bullet, spawnLoation, bulletPos.rotation);
+            Instantiate(bullet, spawnLoation, turretPos.rotation);
         
     }
     
